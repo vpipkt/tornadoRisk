65e82f910fc69ab504c32284c2a499e93ef1e5e8
Jason Brown <brown27@gmail.com>
2014-10-14 20:58:43 -0400

    basic read data

--- DataForAnalysis/rawDataScript.R
+++ DataForAnalysis/rawDataScript.R
@@ -23,6 +23,7 @@
 #end lat
 # end lon
 #track length miles
+#track width in yards (presume maximum ?)
 #confusing number of states field
 #confusing state number field
 #confusing tornado segment field --  for touchdowns, get only records = 1; 2 are dupes with state detail. -9 are dupes with additional counties
@@ -32,4 +33,24 @@
 #county FIPS 4
 
 
+#URL working & correct as of October 2014
+url<-"http://www.spc.noaa.gov/wcm/data/1950-2013_torn.csv"
+url<-"c:/users/jason/source/repos/tornado/tornadoRisk/RawData/1950-2013_torn.csv"
+#cheat for developing these functions
+
+torn<- read.csv(url,header=FALSE,
+             col.names=c("obsInYear","year","month","day","date","time",
+                         "time.zone","state.post","state.fips","state.storm",
+                         "f.scale","injury.count","fatality.count","loss",
+                         "crop.loss","start.lat","start.lon","end.lat","end.lon",
+                         "track.len.mi","track.wid.yd","state.confusing","state.confusing.2","segment",
+                         "county.fips.1","county.fips.2","county.fips.3","county.fips.4"),
+             colClasses=c(rep("numeric",4),"character","character",
+                          rep("factor",3),"numeric",
+                          "factor",rep("numeric",10),
+                          rep("factor",7)))
+                     
+#need to seriously clean for nulls 
+# several instances -9 is used for this
+#lat lon if 0, must be na
 
