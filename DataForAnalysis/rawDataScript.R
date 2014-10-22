27054d0100dedac88262c7f65849a2482b5fcdb7
Jason Brown <jbrown@groupw.com>
2014-10-22 14:43:18 -0400

    More cleaning and coding

--- DataForAnalysis/rawDataScript.R
+++ DataForAnalysis/rawDataScript.R
@@ -35,7 +35,7 @@
 
 #URL working & correct as of October 2014
 url<-"http://www.spc.noaa.gov/wcm/data/1950-2013_torn.csv"
-url<-"c:/users/jason/source/repos/tornado/tornadoRisk/RawData/1950-2013_torn.csv"
+url<-"c:/users/jbrown/source/repos/tornado/RawData/1950-2013_torn.csv"
 #cheat for developing these functions
 
 torn<- read.csv(url,header=FALSE,
@@ -56,7 +56,34 @@ torn<- read.csv(url,header=FALSE,
 summary(torn)
 
 #date & time to R date format
-# do
+torn$date2 <- as.Date(torn$date, "%Y-%m-%d")
+s<-sample(1:nrow(torn),100)
+cbind(torn$date[s],format(torn$date2[s]))
+rm(s)
+
+#states look fine.
+summary(torn$state.post)
 
 #fscale
 torn$f.scale[torn$f.scale==-9] <- NA
+
+#loss needs significant work as it was re-coded multiple times
+summary(torn$loss)
+torn$loss[torn$loss == 0] <- NA
+#easy way to do this is to code all as a factor,including newer ones with $M estimates
+torn$loss.cat <- ordered(NA,levels=1:9,
+                         labels= c("<50","50-500","500-5,000","5k-50k","50k-500k","500k-5M",
+                                   "5M-50M","50M-500M",">500M"))
+              
+sub<- torn$year<1996 & !is.na(torn$loss)
+torn$loss.cat[sub] <-torn$loss[sub]
+
+#may be easier to do this by assigning midpoints as a point est for pre-'96, then set loss.cat with "cut"
+
+loss.point<- data.frame(loss=1:9,lb=c(0,50,500,5000,50000,500000, 5000000, 50000000,500000000),
+                        ub=c(50,500,5000,50000,500000, 5000000, 50000000,500000000,NA))
+#compute geometric mean of bucket
+loss.point$point.est = sqrt(loss.point$lb * loss.point$ub)
+loss.point$point.est[9]=500000000
+
+#merge loss.point with torn to set pre '96 point estimates
